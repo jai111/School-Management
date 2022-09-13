c8361699e09a390d4180f61964b5d967eee7e4cd
jai111 <bajpaijai51@gmail.com>
2022-09-13 11:18:48 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -34,19 +34,6 @@ app.use('/api/students', require('./routes/students'));
 app.use('/api/teachers', require('./routes/teachers'));
 
 
-app.use('/uploads', express.static('uploads'));
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-
- 
-  app.use(express.static("client/build"));
-
- 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
-}
 
 const port = process.env.PORT || 5000
 
